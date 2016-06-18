e98d7b126f7a3b16773e55d7a84d35a5df380b71
Nima Hejazi <nima.hejazi7@gmail.com>
2016-06-17 17:49:29 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,13 +5,52 @@
 #######===========================#######
 --]]
 
--- simple "Hello World!" example
---[[ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World!"}):send()
-end) --]]
+-- Toggle an application between being frontmost app and hidden
+function toggle_application(_app)
+    local app = hs.appfinder.appFromName(_app)
+    if not app then
+        -- FIXME: This should really launch _app
+        return
+    end
+    local mainwin = app:mainWindow()
+    if mainwin then
+        if mainwin == hs.window.focusedWindow() then
+            mainwin:application():hide()
+        else
+            mainwin:application():activate(true)
+            mainwin:application():unhide()
+            mainwin:focus()
+        end
+    end
+end
+
+
+-- Perform tasks to configure the system for my home WiFi network
+function home_arrived()
+    hs.audiodevice.defaultOutputDevice():setVolume(25)
+    hs.notify.new({
+          title='Hammerspoon',
+            informativeText='unmuted volume'
+        }):send()
+end
+
+
+-- Perform tasks to configure the system for any WiFi network other than my home
+function home_departed()
+    hs.audiodevice.defaultOutputDevice():setVolume(0)
+    hs.notify.new({
+          title='Hammerspoon',
+            informativeText='muted volume'
+        }):send()
+end
+
+
+-- variables for hotkey smash
+local smash = {"cmd", "alt", "ctrl"}
+local altsmash = {"cmd", "alt", "shift"}
 
 -- basic function for window movement (using NetHack key definitions)
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Y", function()
+hs.hotkey.bind(smash, "Y", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
 
@@ -20,7 +59,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Y", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K", function()
+hs.hotkey.bind(smash, "K", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
 
@@ -28,7 +67,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
+hs.hotkey.bind(smash, "U", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
 
@@ -37,7 +76,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+hs.hotkey.bind(smash, "H", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
 
@@ -45,7 +84,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
+hs.hotkey.bind(smash, "L", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
 
@@ -53,7 +92,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "B", function()
+hs.hotkey.bind(smash, "B", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
 
@@ -62,7 +101,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "B", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", function()
+hs.hotkey.bind(smash, "J", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
 
@@ -70,7 +109,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", function()
+hs.hotkey.bind(smash, "N", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
 
@@ -80,7 +119,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", function()
 end)
 
 -- window tiling: splitting panes right/left for productivity
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+hs.hotkey.bind(smash, "Right", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -93,7 +132,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+hs.hotkey.bind(smash, "Left", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -152,94 +191,75 @@ end
 
 -- reactions to WiFi network changes
 local wifiWatcher = nil
-local homeSSID = "Knock_On_101" --may need updating
+local homeSSID = "Knock_On_101" --NEEDS UPDATING
 local lastSSID = hs.wifi.currentNetwork()
 
 function ssidChangedCallback()
     newSSID = hs.wifi.currentNetwork()
 
+    print("ssidChangedCallback: old:"..(lastSSID or "nil").." new:"..(newSSID or "nil"))
     if newSSID == homeSSID and lastSSID ~= homeSSID then
         -- We just joined our home WiFi network
-        hs.audiodevice.defaultOutputDevice():setVolume(25)
+        home_arrived()
     elseif newSSID ~= homeSSID and lastSSID == homeSSID then
         -- We just departed our home WiFi network
-        hs.audiodevice.defaultOutputDevice():setVolume(0)
+        home_departed()
     end
 
     lastSSID = newSSID
 end
 
-wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
-wifiWatcher:start()
 
 -- controlling Spotify (iTunes has dedicated buttons on Mac)
-hs.hotkey.bind({"cmd", "alt", "shift"}, "2", function()
-  hs.spotify.play()
-end)
+hs.hotkey.bind(altsmash, "2", function() hs.spotify.play() end)
 
-hs.hotkey.bind({"cmd", "alt", "shift"}, "`", function()
-  hs.spotify.pause()
-end)
+hs.hotkey.bind(altsmash, "`", function() hs.spotify.pause() end)
 
-hs.hotkey.bind({"cmd", "alt", "shift"}, "3", function()
-  hs.spotify.next()
-end)
+hs.hotkey.bind(altsmash, "3", function() hs.spotify.next() end)
 
-hs.hotkey.bind({"cmd", "alt", "shift"}, "1", function()
-  hs.spotify.previous()
-end)
+hs.hotkey.bind(altsmash, "1", function() hs.spotify.previous() end)
 
-hs.hotkey.bind({"cmd", "alt", "shift"}, "4", function()
-  hs.spotify.displayCurrentTrack()
-end)
+hs.hotkey.bind(altsmash, "4", function() hs.spotify.displayCurrentTrack() end)
 
 -- launch shortcuts for useful applications
 -- NOTE {"cmd", "alt", "ctrl"}, '4' reserved for dash global key
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "-", function()
-  hs.application.launchOrFocus("iTerm")
-end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "=", function()
-  hs.application.launchOrFocus("Atom")
-end)
+hs.hotkey.bind(smash, "-", function() toggle_application("iTerm") end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "[", function()
-  hs.application.launchOrFocus("Google Chrome")
-end)
+hs.hotkey.bind(smash, "=", function() toggle_application("Atom") end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "]", function()
-  hs.application.launchOrFocus("Finder")
-end)
+hs.hotkey.bind(smash, "[", function() toggle_application("Google Chrome") end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
-  hs.application.launchOrFocus("Kiwi for Gmail")
-end)
+hs.hotkey.bind(smash, "]", function() toggle_application("Finder") end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "P", function()
-  hs.application.launchOrFocus("Preview")
-end)
+hs.hotkey.bind(smash, "M", function() toggle_application("Kiwi for Gmail") end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "V", function()
-  hs.application.launchOrFocus("MacVim")
-end)
+hs.hotkey.bind(smash, "P", function() toggle_application("Preview") end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
-  hs.application.launchOrFocus("RStudio")
-end)
+hs.hotkey.bind(smash, "V", function() toggle_application("MacVim") end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "S", function()
-  hs.application.launchOrFocus("Spotify")
-end)
+hs.hotkey.bind(smash, "R", function() toggle_application("RStudio") end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", function()
-  hs.application.launchOrFocus("Messenger")
-end)
+hs.hotkey.bind(smash, "S", function() toggle_application("Spotify") end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", function()
-  hs.application.launchOrFocus("Messages")
-end)
+hs.hotkey.bind(smash, "F", function() toggle_application("Messenger") end)
+
+hs.hotkey.bind(smash, "T", function() toggle_application("Messages") end)
 
 -- hints for shortcuts assigned
 --[[ hs.hotkey.bind({"cmd", "alt", "ctrl"}, ";", function()
   hs.hotkey.showHotkeys()
 end) --]]
+
+
+-- use information about changing Wifi networks
+wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
+wifiWatcher:start()
+
+
+-- Make sure we have the right location settings
+if hs.wifi.currentNetwork() == homeSSID then
+    home_arrived()
+else
+    home_departed()
+end
